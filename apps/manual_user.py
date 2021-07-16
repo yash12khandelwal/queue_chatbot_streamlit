48c8ad223a49490fca4b9ce325fb6668abe5fbac
Yash Khandelwal <yash12khandelwal@gmail.com>
2021-07-17 04:10:17 +0530

    manual user entry using text box

--- apps/manual_user.py
+++ apps/manual_user.py
@@ -26,23 +26,26 @@ def app():
     st.title('Enter User in queue manually')
 
     mobile_number = st.text_input("Enter Mobile Number")
+    mobile_number += "@c.us"
     if mobile_number:
-        add_manual_user("doctor", mobile_number)
+        text = st.text_input("Enter text")
+        st.write(add_manual_user(text, mobile_number))
+        # add_manual_user("doctor", mobile_number)
         
-        if st.button('Book'):
-            aadhaar = st.text_input("Enter Aadhar Number")
-            cowin_ref_id = st.text_input("Enter Cowin Reference Id")
-            jewellers_id = st.text_input("Enter Jewellers Association Id")
-            resp = add_manual_user("1", mobile_number)
-            resp = add_manual_user(aadhaar)
-            resp = add_manual_user(cowin_ref_id)
-            resp = add_manual_user(jewellers_id)
-            st.success(resp)
+        # if st.button('Book'):
+        #     aadhaar = st.text_input("Enter Aadhar Number")
+        #     cowin_ref_id = st.text_input("Enter Cowin Reference Id")
+        #     jewellers_id = st.text_input("Enter Jewellers Association Id")
+        #     resp = add_manual_user("1", mobile_number)
+        #     resp = add_manual_user(aadhaar, mobile_number)
+        #     resp = add_manual_user(cowin_ref_id, mobile_number)
+        #     resp = add_manual_user(jewellers_id, mobile_number)
+        #     st.success(resp)
 
-        if st.button("Status"):
-            resp = add_manual_user("1", mobile_number)
-            st.success(resp)
+        # if st.button("Status"):
+        #     resp = add_manual_user("1", mobile_number)
+        #     st.success(resp)
 
-        if st.button("Cancel"):
-            resp = add_manual_user("2", mobile_number)
-            st.success(resp)
+        # if st.button("Cancel"):
+        #     resp = add_manual_user("2", mobile_number)
+        #     st.success(resp)
